9ad3061852ca9821920c6b500e94135cb0d458fe
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2020-02-25 21:25:47 -0700

    added requests for posts to database with KNEX and using sqlite3

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,26 +1,78 @@
-const express = require("express")
-const db = require("../data/config")
+const express = require("express");
+const db = require("../data/config");
 
-const router = express.Router()
+const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-
-})
+ try {
+  // translates to "SELECT * FROM posts"
+  const posts = await db.select("*").from("posts");
+  res.json(posts);
+ } catch (err) {
+  next(err);
+ }
+});
 
 router.get("/:id", async (req, res, next) => {
-
-})
+ try {
+  // translates to "SELECT * FROM posts WHERE id = ? LIMIT 1;"
+  const post = await db
+   .first("*")
+   .from("posts")
+   .where("id", req.params.id)
+   .limit(1);
+  res.json(post);
+ } catch (err) {
+  next(err);
+ }
+});
 
 router.post("/", async (req, res, next) => {
-
-})
+ try {
+  const payload = {
+   title: req.body.title,
+   contents: req.body.contents
+  };
+  // translates to "INSERT INTO posts (title, contents) VALUES (?,?);"
+  const [id] = await db("posts").insert(payload);
+  const newPost = await db("posts")
+   .where("id", id)
+   .first();
+  res.json(newPost);
+ } catch (err) {
+  next(err);
+ }
+});
 
 router.put("/:id", async (req, res, next) => {
-
-})
+ try {
+  const payload = {
+   title: req.body.title,
+   contents: req.body.contents
+  };
+  // translates to "UPDATE posts SET ? = ? WHERE id = ?"
+  await db("posts")
+   .where("id", req.params.id)
+   .update(payload);
+  const post = await db("posts")
+   .where("id", req.params.id)
+   .first();
+  res.json(post);
+ } catch (err) {
+  next(err);
+ }
+});
 
 router.delete("/:id", async (req, res, next) => {
+ try {
+  // translates to "DELETE FROM posts WHERE id = ?"
+  await db("posts")
+   .where("id", req.params.id)
+   .del();
+  res.status(204).end();
+ } catch (err) {
+  next(err);
+ }
+});
 
-})
-
-module.exports = router
+module.exports = router;
